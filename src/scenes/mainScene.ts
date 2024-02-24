31e9652f40eeb13bdef48f1ea2b5a3f5838120ea
Mitchell <mramosna@udel.edu>
2024-02-24 15:59:49 -0500

    changes

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,20 +1,33 @@
 import Phaser from "phaser";
 
 export default class MainScene extends Phaser.Scene {
+    private platforms?: Phaser.Physics.Arcade.StaticGroup;
     constructor() {
         super({ key: "MainScene" });
     }
 
     create() {
         this.add.image(400, 300, "sky");
+        this.add.image(400, 300, "star");
+        this.platforms = this.physics.add.staticGroup();
+        const ground = this.platforms.create(
+            400,
+            568,
+            "ground"
+        ) as Phaser.Physics.Arcade.Sprite;
+        ground.setScale(2).refreshBody();
 
-        const message = `Phaser v${Phaser.VERSION}`;
+        this.platforms.create(600, 400, "ground");
+        this.platforms.create(50, 250, "ground");
+        this.platforms.create(750, 220, "ground");
+
+        /*const message = `Phaser v${Phaser.VERSION}`;
         this.add
             .text(this.cameras.main.width - 15, 15, message, {
                 color: "#000000",
                 fontSize: "24px",
             })
-            .setOrigin(1, 0);
+            .setOrigin(1, 0);*/
     }
 
     update() {}
